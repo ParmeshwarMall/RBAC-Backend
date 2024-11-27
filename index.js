35a01be5e484adcb0de43f231bccbf8b3f43f4e3
ParmeshwarMall <142672221+ParmeshwarMall@users.noreply.github.com>
2024-11-27 18:07:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin:"https://rbac.netlify.app",
+    origin:"https://role-access.netlify.app",
     credentials: true,
   })
 );
